f3c7fb81ff84d1b8bfc18f92833e9d6dce2ce8a7
Erik Dubbelboer <erik@dubbelboer.com>
2026-03-07 19:33:22 +0900

    Improve Vite chunk splitting for vendor dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,20 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "./"
+  base: "./",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules/three")) {
+            return "vendor-three";
+          }
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+          return undefined;
+        }
+      }
+    }
+  }
 });
